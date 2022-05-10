5749fb1d47dab828d59b2dadc80758eac8c80f6b
Sonit2000 <hoangson.it2000@gmail.com>
2022-05-10 09:27:05 +0700

    update frontend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,25 +11,25 @@ const crypto = require("crypto");
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
 
 
-    const { name, email, password } = req.body;
+  const { name, email, password } = req.body;
 
-    const user = await User.create({
-        name,
-        email,
-        password,
-        avatar: {
-          public_id: "this is a sample id",
-          url: "profilepicUrl",
-        },
-      });
+  const user = await User.create({
+    name,
+    email,
+    password,
+    avatar: {
+      public_id: "this is a sample id",
+      url: "profilepicUrl",
+    },
+  });
 
-      sendToken(user, 201, res);
-      // const token = user.getJWTToken();
+  sendToken(user, 201, res);
+  // const token = user.getJWTToken();
 
-      // res.status(201).json({
-      //     success: true,
-      //     token,
-      // });
+  // res.status(201).json({
+  //     success: true,
+  //     token,
+  // });
 });
 
 // Login User
@@ -44,7 +44,7 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
 
   const user = await User.findOne({ email }).select("+password");
 
-  
+
   if (!user) {
     return next(new ErrorHander("Invalid email or password", 401));
   }
@@ -61,7 +61,7 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
 
 
 // Logout User
-exports.logout = catchAsyncErrors(async (req, res, next) =>{
+exports.logout = catchAsyncErrors(async (req, res, next) => {
 
   res.cookie("token", null, {
     expires: new Date(Date.now()),
@@ -190,18 +190,18 @@ exports.updateProfile = catchAsyncErrors(async (req, res, next) => {
     email: req.body.email,
   };
 
-//
+  //
   const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
     new: true,
     runValidators: true,
     useFindAndModify: false,
-  }) 
-  
+  })
+
   res.status(200).json({
     success: true,
   });
 
-}); 
+});
 
 // Get all users(admin)
 exports.getAllUser = catchAsyncErrors(async (req, res, next) => {
@@ -228,7 +228,7 @@ exports.getSingleUser = catchAsyncErrors(async (req, res, next) => {
     success: true,
     user,
   });
-}); 
+});
 
 // update User Role -- Admin
 exports.updateUserRole = catchAsyncErrors(async (req, res, next) => {
--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,36 @@
 const app = require("./app");
+var morgan = require('morgan')
 
 const dotenv = require("dotenv");
 const connectDatabase = require("./config/database")
 
+app.use(morgan('combined'))
+
 //Handling Uncaught Exception
 process.on("uncaughtException", (err) => {
-     console.log(`Error: ${err.message}`);
-     console.log(`Shutting down the server due to Uncaught Exception`);
-     process.exit(1);
-   });
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Uncaught Exception`);
+  process.exit(1);
+});
 
 // Config
-dotenv.config({path: "backend/config/config.env"});
+dotenv.config({ path: "backend/config/config.env" });
 
 //connecting to database
 connectDatabase();
 
-const server = app.listen(process.env.PORT,()=>{
-     console.log(`Server is working on http://localhost:${process.env.PORT}`);
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server is working on http://localhost:${process.env.PORT}`);
 });
 
 
 
 //unhandled promise rejections
 process.on("unhandledRejection", (err) => {
-     console.log(`Error: ${err.message}`);
-     console.log(`Shutting down the server due to Unhandled Promise Rejection`);
-   
-     server.close(() => {
-       process.exit(1);
-     });
-   });
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
